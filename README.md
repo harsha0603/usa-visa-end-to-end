a51c363e2cf8f5a030d782a47a3d9f12cf378992
harsha0603 <harshamassss@gmail.com>
2024-09-11 13:34:35 +0530

    data ingestion added

--- README.md
+++ README.md
@@ -15,4 +15,12 @@ env/Scripts/activate
 pip install -r requirements.txt
 ```
 
+## Workflow
+
+1. constants
+2. entity
+3. components
+4. pipeline
+
+
 - 
--- demo.py
+++ demo.py
@@ -1,10 +1,20 @@
-from us_visa.logger import logging
-from us_visa.exception import USvisaException
-import sys
-logging.info("Testing logging code successfull")
+# from us_visa.logger import logging
+# from us_visa.exception import USvisaException
+# import sys
+# logging.info("Testing logging code successfull")
 
 
-try:
-    a = 7/0
-except Exception as e:
-    raise USvisaException(e,sys)
+# try:
+#     a = 7/0
+# except Exception as e:
+#     raise USvisaException(e,sys)
+# import os 
+
+
+# mongo_db_url = os.getenv('MONGODB_URL')
+# print(mongo_db_url)
+
+# from us_visa.pipline.training_pipeline import TrainPipeline
+
+# obj = TrainPipeline()
+# obj.run_pipeline()
